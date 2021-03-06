7be24a95017a1b402e6cc5f15ee4066c659d1f43
Tobias <devel@zukuul.de>
2021-03-06 18:02:04 +0100

    update rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 git checkout v2.2.x
-
-docker exec -t docker_postgres_1 pg_dumpall -c -U postgres > /srv/docker/greenlight_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-
 git fetch upstream
 git merge upstream/v2.2.x
+
+git checkout behind-nginx
+git log v2.2.x..behind-nginx
+
+echo "merge v2.2.x into behind-nginx? CTRL-C to cancel"
+read
+
+git merge v2.2.x
+
+#docker exec -t docker_postgres_1 pg_dumpall -c -U postgres > /srv/docker/greenlight_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 ./scripts/upgrade
 
